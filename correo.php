6bb271bdba06ff83d490dd2139a0db53663af7f0
JuanCantor <juantrollhdx8@gmail.com>
2020-09-25 14:56:20 -0500

    primer commit

---
+++ correo.php
@@ -0,0 +1,16 @@
+<?php
+    $destinatario = "moonassistantcontact@gmail.com"
+    //esto es correo del destinatario
+    $nombre = $_POST ['nombre'];
+    $apellido = $_POST ['apellido'];
+    $asunto = $_POST ['asunto'];
+    $email = $_POST ['email'];
+    $mensaje = $_POST ['mensaje'];
+
+    $header = "Enviado Desde Pagina MOON";
+    $mensajeCompleto = $mensaje . "\nAtentamente" . $nombre
+    
+    mail($destinatario, $asunto, $mensajeCompleto, $header);
+    echo "<script>alert('Correo Enviado Correctamente')</script>";
+    echo "<script>setTimeout(\"location.href='index.html'\",1000)</script>";
+?>
